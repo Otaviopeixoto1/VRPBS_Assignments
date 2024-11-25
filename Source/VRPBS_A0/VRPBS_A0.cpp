f0291b96cb11869025a447179922d52b156a0f93
otavio <otaviopeixotogm@gmail.com>
2024-11-25 14:34:10 +0100

    Assignment2: Materials

--- Source/VRPBS_A0/VRPBS_A0.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VRPBS_A0.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRPBS_A0, "VRPBS_A0" );
